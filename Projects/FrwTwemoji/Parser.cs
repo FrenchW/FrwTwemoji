aaab9607020f504f46428634958874e178c5db21
Frederic Druilhet <Frederic.Druilhet@dr14.cnrs.fr>
2014-11-11 23:15:58 +0100

    Base projects

---
+++ Projects/FrwTwemoji/Parser.cs
@@ -0,0 +1,12 @@
+﻿
+namespace FrwTwemoji
+{
+    public static class Parser
+    {
+        public static string ParseEmoji(string fromString)
+        {
+
+            return string.Empty;
+        }
+    }
+}
---
+++ Projects/FrwTwemoji/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de 
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("FrwTwemoji")]
+[assembly: AssemblyDescription("Twitter open-sourced emoji icons incorporated in any dotnet project")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("FrenchW")]
+[assembly: AssemblyProduct("FrwTwemoji")]
+[assembly: AssemblyCopyright("Copyright FrenchW ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly 
+// aux composants COM.  Si vous devez accéder à un type dans cet assembly à partir de 
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("3c9cd130-f328-4117-8429-c1b0631b3543")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire 
+//      Numéro de build
+//      Révision
+//
+// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
+// en utilisant '*', comme indiqué ci-dessous :
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
